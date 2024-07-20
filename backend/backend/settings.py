eabe0ad4a55b5504c772b1efc96c148bd7909581
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-07-21 00:33:14 +0530

    mysql connections

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Judge',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
