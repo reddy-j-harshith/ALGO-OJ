5097b3b7948753904385e56564d2c4a9be450909
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-07-21 09:49:12 +0530

    registration page

--- frontend/src/pages/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage.js
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const RegistrationPage = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let response = await fetch('http://localhost:8000/api/register/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    let data = await response.json();
+
+    if (response.status === 200) {
+      // Registration successful
+    } else if (response.status === 401) {
+      // Unauthorized
+    }
+    console.log(formData);
+  };
+
   return (
     <div>
-      
+      <h2>Registration Page</h2>
+      <form onSubmit={handleSubmit}>
+      <label>
+          Username :
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstname"
+            value={formData.firstname}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default RegistrationPage
+export default RegistrationPage;
