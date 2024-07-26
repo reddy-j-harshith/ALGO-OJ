733fd1dd5baae7848c9cbcd5942024347226bb87
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-07-26 14:55:34 +0530

    Docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,17 +79,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'judge',
+#         'USER': 'admin',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',  # Change to your container's service name if different
+#         'PORT': '3306',  # Leave blank for socket connection
+#         'OPTIONS': {
+#             'unix_socket': '/run/mysqld/mysqld.sock',
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Judge',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
