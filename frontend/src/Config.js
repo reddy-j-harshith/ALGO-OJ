bee06c69451a21c5bf8e8690dd3ae99788063c31
reddy-j-harshith <reddy.j.harshith@gmail.com>
2024-08-01 01:26:45 +0530

    backend https

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const Config = {
-    baseURL: process.env.REACT_APP_DJANGO_BASE_URL || 'http://43.205.99.1:8000',
+    baseURL: process.env.REACT_APP_DJANGO_BASE_URL || 'https://backend.algorithmix.online',
 };
 
 // const Config = {
