5305a41656789168f6bc37b07db7f473f994704e
reddy-j-harshith <enactus@hyderabad.bits-pilani.ac.in>
2024-08-06 02:08:42 +0530

    f

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,11 +1,11 @@
 // src/config.js
-// const Config = {
-//     baseURL: process.env.REACT_APP_DJANGO_BASE_URL || 'https://backend.algorithmix.online',
-// };
-
 const Config = {
-    baseURL: process.env.REACT_APP_DJANGO_BASE_URL || 'http://localhost:8000',
+    baseURL: process.env.REACT_APP_DJANGO_BASE_URL || 'https://backend.algorithmix.online',
 };
+
+// const Config = {
+//     baseURL: process.env.REACT_APP_DJANGO_BASE_URL || 'http://localhost:8000',
+// };
 
 
 export default Config;
